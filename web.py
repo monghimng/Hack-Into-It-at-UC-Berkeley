33c504ae414296b7a502716b7ce8906283f2550a
Sayan-RC <sayanrayc@gmail.com>
2016-09-17 15:18:33 -0700

    Delete web.py

--- web.py
+++
@@ -1,4 +0,0 @@
-import urllib.request as web
-link = "http://api.census.gov/data/2010/sf1?key=4dbe021cda8a6bb5a803fe065758760671f13e3f&get=P0010001,NAME&for=state:41"
-f = web.urlopen(link)
-print(f.read())
