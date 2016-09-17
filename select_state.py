86911689d316f647547e0c987d49d2a237892e9a
Mong H. Ng <monghim.ng@berkeley.edu>
2016-09-17 15:22:41 -0700

    finished!

--- select_state.py
+++ select_state.py
@@ -1,30 +1,6 @@
 import requests
 import csv
-
-def is_valid_state_num(num):
-    lst = [3, 7, 43]
-    return type(num)==type(5) and num not in lst and (num > 0 and num < 57 or num == 72)
-
-def select_state():
-    params = {
-        'get': 'NAME',
-        'for': 'state:*',
-        'key': '65574da907a7e319b5fabb998098789b74e2092a'
-    }
-    data = requests.get("http://api.census.gov/data/2015/acs1", params=params)
-
-    print('Please select your desired state to work in.')
-    for state in data.json()[1:]:
-        print(state[1], state[0])
-    workplace = int(input())
-    while not is_valid_state_num(workplace):
-        workplace = int(input('Not a valid state input. Please try again.'))
-    print('-----------')
-	return workplace
-
-def select_field_of_work():
-    print('Please select your desired field of work')
-    menu = """*** Fields of Work Menu ***
+menu = """*** Fields of Work Menu ***
 04 - Management occupations
 05 - Business and financial operations occupations
 07 - Computer and mathematical occupations
@@ -49,29 +25,46 @@ def select_field_of_work():
 32 - Installation, maintenance, and repair occupations
 34 - Production occupations
 35 - Transportation occupations
-36 - Material moving occupations"""
+36 - Material moving occupations\n"""
+def is_valid_state_num(num):
+    lst = [3, 7, 43]
+    return type(num)==type(5) and num not in lst and (num > 0 and num < 57 or num == 72)
+
+def select_state():
+    params = {
+        'get': 'NAME',
+        'for': 'state:*',
+        'key': '65574da907a7e319b5fabb998098789b74e2092a'
+    }
+    data = requests.get("http://api.census.gov/data/2015/acs1", params=params)
+
+    print('Please select your desired state to work in.')
+    for state in data.json()[1:]:
+        print(state[1], state[0])
+    workplace = int(input())
+    while not is_valid_state_num(workplace):
+        workplace = int(input('Not a valid state input. Please try again.'))
+    return workplace
+
+def select_field_of_work():
+    print('Please select your desired field of work')
     field = int(input(menu))
     while type(field)!=type(5) or field < 1 or field > 36:
         print('Not a valid Field of work. Please try again.')
         field = int(input())
     return field
-	
-def return_average_salary():
-	occupation = select_field_of_work()
-	state_num = select_state()
-	
-	census_variables = ['NAME', occupation]
-	state = 'state:{}'.format(state_num)
 
-	params = {
-		'get': ','.join(census_variables),
-		'for': state,
-		'key': '65574da907a7e319b5fabb998098789b74e2092a'
-	}
-	
-	data = requests.get("http://api.census.gov/data/2015/acs1", params=params)
-	income = []
-	for row in data.json()[1:]:
-		income.append(float(row[1]))
-	print(income)
-return_average_salary()
+def return_average_salary():
+    occupation = select_field_of_work()
+    state_num = select_state()
+
+    params = {
+        'get': 'B24021_0{:02d}E'.format(occupation),
+        'for': 'state:'+str(state_num),
+        'key': '65574da907a7e319b5fabb998098789b74e2092a'
+    }
+
+    data = requests.get("http://api.census.gov/data/2015/acs1", params=params)
+    return data.json()[1:][0][0]
+result = 'Your expected salary is ${}'.format(return_average_salary())
+print(result)
